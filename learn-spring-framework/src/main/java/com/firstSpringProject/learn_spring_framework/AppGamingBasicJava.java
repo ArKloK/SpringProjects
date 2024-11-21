848fd6700bac09185733b5790c8b0e8899965274
ArKloK <ccamachobanez@gmail.com>
2024-11-21 14:13:14 +0100

    Created new class PacmanGame

--- learn-spring-framework/src/main/java/com/firstSpringProject/learn_spring_framework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/firstSpringProject/learn_spring_framework/AppGamingBasicJava.java
@@ -2,17 +2,19 @@ package com.firstSpringProject.learn_spring_framework;
 
 import com.firstSpringProject.learn_spring_framework.game.GameRunner;
 import com.firstSpringProject.learn_spring_framework.game.MarioGame;
+import com.firstSpringProject.learn_spring_framework.game.PacmanGame;
 import com.firstSpringProject.learn_spring_framework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
 
 	public static void main(String[] args) {
-		
+
 		var marioGame = new MarioGame();
-		
+
 		var superContraGame = new SuperContraGame();
-		
-		var gameRunner = new GameRunner(marioGame);
+		PacmanGame pacmanGame = new PacmanGame();
+
+		var gameRunner = new GameRunner(pacmanGame);
 		gameRunner.run();
 	}
 
