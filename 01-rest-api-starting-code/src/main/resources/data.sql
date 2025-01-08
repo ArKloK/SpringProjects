bda977e51584b336af0191115b29776a2ff82c07
ArKloK <ccamachobanez@gmail.com>
2025-01-08 13:54:29 +0100

    Module 13. Connecting Java Full Stack Application(Spring Boot & React) with JPA & Hibernate

---
+++ 01-rest-api-starting-code/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO TODO (id, description, done, target_date, username) VALUES
+(10001, 'Learn Spring', false, '2023-12-31', 'carlos'),
+(10002, 'Learn Hibernate', false, '2023-12-31', 'carlos'),
+(10003, 'Learn JPA', false, '2023-12-31', 'carlos');
--- frontend/todo-app/src/components/todo/security/AuthContext.jsx
+++ frontend/todo-app/src/components/todo/security/AuthContext.jsx
@@ -22,38 +22,6 @@ export default function AuthProvider({ children }) {
 
     const [token, setToken] = useState(null)
 
-    // async function login(username, password) {
-
-    //     const baToken = 'Basic ' + window.btoa(username + ':' + password);
-
-    //     try {
-    //         const response = await executeBasicAuthenticationService(baToken);
-
-    //         if (response.status === 200) {
-    //             setIsAuthenticated(true);
-    //             setUsername(username);
-    //             setToken(baToken);
-
-    //             apiClient.interceptors.request.use(
-    //                 (config) => {
-    //                     console.log("interceptor - basic auth");
-    //                     config.headers.Authorization = baToken;
-    //                     return config;
-    //                 }
-    //             );
-
-    //             return true;
-    //         } else {
-    //             logout();
-    //             return false;
-    //         }
-    //     } catch (error) {
-    //         logout();
-    //         return false;
-    //     }
-
-    // }
-
     async function login(username, password) {
 
         try {
